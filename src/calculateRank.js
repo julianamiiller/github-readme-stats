6811e2ec7a0c345d1976dfe6747e3e08385567e4
Juliana Miiller <julianamiillergithub@gmail.com>
2026-08-13 16:02:48 -0300

    Hack: fix rank to always return A

--- src/calculateRank.js
+++ src/calculateRank.js
@@ -78,9 +78,9 @@ function calculateRank({
       FOLLOWERS_WEIGHT * log_normal_cdf(followers / FOLLOWERS_MEDIAN)) /
       TOTAL_WEIGHT;
 
-  const level = LEVELS[THRESHOLDS.findIndex((t) => rank * 100 <= t)];
+  const level = "A";
 
-  return { level, percentile: rank * 100 };
+  return { level, percentile: 10 };
 }
 
 export { calculateRank };
